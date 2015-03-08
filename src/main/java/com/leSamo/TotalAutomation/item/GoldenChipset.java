bf8ad1d6fdc64446a68587761b2fcce977447d82
leSamo <le.samosk@gmail.com>
2015-03-08 11:45:25 +0100

    Fixed Chipsets

---
+++ src/main/java/com/leSamo/TotalAutomation/item/GoldenChipset.java
@@ -0,0 +1,9 @@
+package com.leSamo.TotalAutomation.item;
+
+public class GoldenChipset extends TAItem {
+	public GoldenChipset() {
+		super();
+		this.setUnlocalizedName("goldenchipset");
+		this.setTextureName("GoldenChipset");
+	}
+}
